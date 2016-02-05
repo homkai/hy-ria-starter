beaeeb55a6d5a8dd6040905d7e66a1313fe3351c
ZQXie <2944927590@qq.com>
2016-02-05 20:56:41 +0800

    gulpfile.js update
    gulpfile.js提交使得新增和修改文件时，gulp能自动处理

--- gulpfile.js
+++ gulpfile.js
@@ -6,7 +6,8 @@ var del = require('del');
 
 var DIST = './dist/';
 var DIST_APP = DIST + 'app/';
-var SRC = './src/';
+
+var SRC = 'src/';
 var APP = SRC + 'app/';
 
 gulp.task('clean', function () {
@@ -20,6 +21,23 @@ function handleCopy(path) {
     path = typeof path === 'string' ? path : APP + '**/*.js';
     gulp.src(path, {base: APP})
         .pipe(gulp.dest(DIST_APP));
+}
+
+function handleDelete(path){
+    if(typeof path === 'string'){
+        path = path.substr(path.lastIndexOf(SRC.replace('/', '\\'))).replace(SRC.replace('/', ''), DIST);
+        var pathArr = path.split('\\');
+        var arr = pathArr[pathArr.length - 1].split('.');
+        if(path.indexOf('.html') != -1) {
+            pathArr[pathArr.length - 1] = 'tpl_' + arr[0] + '.js';
+        } else if (path.indexOf('.css') != -1) {
+            pathArr[pathArr.length - 1] = 'css_' + arr[0] + '.js';
+        }
+        path = pathArr.join('\\');
+    } else {
+        path = DIST_APP + '**/*.js';
+    }
+    del.sync(path);
 }
 
 // CSS压缩、包装成seajs module
@@ -49,13 +67,27 @@ function handleDoT(path) {
 gulp.task('watch', function (){
     gulp.watch([SRC + '**/*.js', SRC + '**/*.json'], function (vinyl) {
         console.log('watch copy', vinyl);
-        handleCopy(vinyl.path);
+        switch ( vinyl.type ){
+            case 'added' :
+            case 'changed' : handleCopy(vinyl.path); break;
+            case 'deleted' : handleDelete(vinyl.path); break;
+        }
     });
     gulp.watch(APP + '**/*.html', function (vinyl) {
-        handleDoT(vinyl.path);
+        console.log('watch Dot', vinyl);
+        switch ( vinyl.type ){
+            case 'added' :
+            case 'changed' : handleDoT(vinyl); break;
+            case 'deleted' : handleDelete(vinyl.path); break;
+        }
     });
     gulp.watch(APP + '**/*.css', function (vinyl) {
-        handleCss(vinyl.path);
+        console.log('watch Css', vinyl);
+        switch ( vinyl.type ) {
+            case 'added' :
+            case 'changed' : handleCss(vinyl); break;
+            case 'deleted' : handleDelete(vinyl.path); break;
+        }
     });
 });
 
